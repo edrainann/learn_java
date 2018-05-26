6c62de9eda34400fbb4968d005f0fa26670c4437
edrainann <438748968@qq.com>
2018-05-26 18:58:24 +0800

    --

---
+++ Study/src/demo/Biaoshifu.java
@@ -0,0 +1,27 @@
+package demo;
+/*
+测试标识符的写法
+1字节=8，2的八次方=256
+2字节=16，2的十六次方=65536
+javac -encoding UTF-8 Biaoshifu.java
+*/
+public class Biaoshifu {
+    public static void main(String[] args){
+        int $abd = 3;
+        int $ = 4;
+        int _123 = 5;
+        //int 123abc = 7;   //标识符不能以数字开头
+        //int abd# = 3;     //标识符不能包含除了字母、数字、下划线、$ 之外的其他字符
+//        int class = 3；
+        int 哇 = 8;    //java内部采用了Unicode字符集， universal
+        char a1 = '艾';
+        char a2 = 'a';
+        int a3 = a2 + 9;
+        char a4 = (char)a3; //强制转型
+
+        System.out.println(哇);
+        System.out.println(a1);
+        System.out.println(a4);
+
+    }
+}
---
+++ Study/src/demo/Hola.java
@@ -0,0 +1,17 @@
+package demo;
+
+/*
+这是一个hello world的程序
+非常简单的程序
+*/
+
+public class Hola {
+    public static void main(String[] args){     //arg是arguments的缩写
+            System.out.println("Hello world");
+            System.out.println("xxxxx");
+    }
+}
+
+class Welcome{
+
+}
